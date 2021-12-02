3a2895b74b5745db95ca873b3396ceabecb1d11e
Junshick <hellojun12@naver.com>
2021-12-02 22:16:56 +0900

    deploy

--- main.py
+++ main.py
@@ -215,18 +215,18 @@ async def on_shutdown(app):
     parser = argparse.ArgumentParser(
         description="WebRTC audio / video / data-channels demo"
     )
-#     parser.add_argument("--cert-file", help="SSL certificate file (for HTTPS)")
-#     parser.add_argument("--key-file", help="SSL key file (for HTTPS)")
-#     parser.add_argument(
-#         "--host", default="0.0.0.0", help="Host for HTTP server (default: 0.0.0.0)"
-#     )
-#     parser.add_argument(
-#         "--port", type=int, default=8080, help="Port for HTTP server (default: 8080)"
-#     )
-#     parser.add_argument(
-#         "--model_dir", type=str, default="./model/pushup_model/body_language_lr.pkl", help="Saved model directory"
-#     )
-#     parser.add_argument("--verbose", "-v", action="count")
+    parser.add_argument("--cert-file", help="SSL certificate file (for HTTPS)")
+    parser.add_argument("--key-file", help="SSL key file (for HTTPS)")
+    parser.add_argument(
+        "--host", default="0.0.0.0", help="Host for HTTP server (default: 0.0.0.0)"
+    )
+    parser.add_argument(
+        "--port", type=int, default=8080, help="Port for HTTP server (default: 8080)"
+    )
+    parser.add_argument(
+        "--model_dir", type=str, default="./model/pushup_model/body_language_lr.pkl", help="Saved model directory"
+    )
+    parser.add_argument("--verbose", "-v", action="count")
 
     args = parser.parse_args()
 
