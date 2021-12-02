a0945b7450d1f3d71258396fb5000152d8da3302
Junshick <hellojun12@naver.com>
2021-12-02 21:51:12 +0900

    deploy real final

--- main.py
+++ main.py
@@ -45,7 +45,7 @@ class VideoTransformTrack(MediaStreamTrack):
         super().__init__()  # don't forget this!
         self.track = track
         self.transform = transform
-        self.detector = pm.poseDetector(model_dir=args.model_dir)
+        self.detector = pm.poseDetector(model_dir="./model/pushup_model/body_language_lr.pkl")
         self.pTime = 0
         self.cnt = 0
     async def recv(self):
@@ -211,37 +211,37 @@ async def on_shutdown(app):
     pcs.clear()
 
 
-if __name__ == "__main__":
-    parser = argparse.ArgumentParser(
-        description="WebRTC audio / video / data-channels demo"
-    )
-    parser.add_argument("--cert-file", help="SSL certificate file (for HTTPS)")
-    parser.add_argument("--key-file", help="SSL key file (for HTTPS)")
-    parser.add_argument(
-        "--host", default="0.0.0.0", help="Host for HTTP server (default: 0.0.0.0)"
-    )
-    parser.add_argument(
-        "--port", type=int, default=8080, help="Port for HTTP server (default: 8080)"
-    )
-    parser.add_argument(
-        "--model_dir", type=str, default="./model/pushup_model/body_language_lr.pkl", help="Saved model directory"
-    )
-    parser.add_argument("--verbose", "-v", action="count")
+# if __name__ == "__main__":
+#     parser = argparse.ArgumentParser(
+#         description="WebRTC audio / video / data-channels demo"
+#     )
+#     parser.add_argument("--cert-file", help="SSL certificate file (for HTTPS)")
+#     parser.add_argument("--key-file", help="SSL key file (for HTTPS)")
+#     parser.add_argument(
+#         "--host", default="0.0.0.0", help="Host for HTTP server (default: 0.0.0.0)"
+#     )
+#     parser.add_argument(
+#         "--port", type=int, default=8080, help="Port for HTTP server (default: 8080)"
+#     )
+#     parser.add_argument(
+#         "--model_dir", type=str, default="./model/pushup_model/body_language_lr.pkl", help="Saved model directory"
+#     )
+#     parser.add_argument("--verbose", "-v", action="count")
 
-    args = parser.parse_args()
+#     args = parser.parse_args()
 
-    if args.verbose:
-        logging.basicConfig(level=logging.DEBUG)
-    else:
-        logging.basicConfig(level=logging.INFO)
+#     if args.verbose:
+#         logging.basicConfig(level=logging.DEBUG)
+#     else:
+#         logging.basicConfig(level=logging.INFO)
 
-    if args.cert_file:
-        ssl_context = ssl.SSLContext()
-        ssl_context.load_cert_chain(args.cert_file, args.key_file)
-    else:
-        ssl_context = None
+#     if args.cert_file:
+#         ssl_context = ssl.SSLContext()
+#         ssl_context.load_cert_chain(args.cert_file, args.key_file)
+#     else:
+#         ssl_context = None
 
-    uvicorn.run(app, host="0.0.0.0", port=args.port)
+#     uvicorn.run(app, host="0.0.0.0", port=args.port)
     # app = web.Application()
     # app.on_shutdown.append(on_shutdown)
     # app.router.add_get("/", index)
